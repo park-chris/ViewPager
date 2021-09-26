9c0570d4067880712fa4cb765607392cb9e6803f
박수정 <zxzx4342@naver.com>
2021-09-26 13:37:41 +0900

    프래그먼트 메이징 담당 어댑터 자업중  - 상속 / 생성자 => 기초 작업  - 필수 구현 함수 2개 오버라이딩

---
+++ app/src/main/java/com/neppplus/viewpager_20210926/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.neppplus.viewpager_20210926.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+}
