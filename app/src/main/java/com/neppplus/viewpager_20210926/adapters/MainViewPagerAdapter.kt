d2dc2a120555e8d0186e18f51c0e0256481b9879
박수정 <zxzx4342@naver.com>
2021-09-26 13:41:34 +0900

    어댑터 함수 2개 작성  - 몇장 짜리인지 알려줌  - 각 위치에 어떤 Fragment가 나가는지.

--- app/src/main/java/com/neppplus/viewpager_20210926/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20210926/adapters/MainViewPagerAdapter.kt
@@ -3,13 +3,25 @@ package com.neppplus.viewpager_20210926.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.viewpager_20210926.fragments.AgeFragment
+import com.neppplus.viewpager_20210926.fragments.GreetingFragment
+import com.neppplus.viewpager_20210926.fragments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
 
+        return 3
     }
 
     override fun getItem(position: Int): Fragment {
-
+        if (position == 0) {
+            return GreetingFragment()
+        }
+        else if (position == 1 ) {
+            return NameFragment()
+        }
+        else {
+            return AgeFragment()
+        }
     }
 }
